535eec2740b2f5566aad83882db69cf3877c2c96
wot narg <wot.narg@gmail.com>
2011-04-11 01:50:24 +0200

    Fixed a linker error that caused everything to fail.

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
  #!/bin/sh
 
+OPTIONS="-Wall -Wextra -nostdlib -nostartfiles -ffreestanding -std=gnu99"
+
 CC="arm-elf-gcc-4.5"
 
  arm-elf-as -o start.o start.s && \
@@ -8,5 +10,6 @@ $CC $OPTIONS -c kio.c -o kio.o && \
 $CC $OPTIONS -c stdlib.c -o stdlib.o && \
 $CC $OPTIONS -c mem.c -o mem.o && \
 $CC $OPTIONS -c sched.c -o sched.o && \
- arm-elf-ld  -T link.ld -o kernel start.o main.o stdlib.o kio.o mem.o sched.o && \
+$CC $OPTIONS -c user.c -o user.o && \
+ arm-elf-ld  -T link.ld -o kernel start.o user.o main.o stdlib.o kio.o mem.o sched.o && \
 gxemul  -VT -E testarm kernel
